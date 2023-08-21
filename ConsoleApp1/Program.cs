8a8d1e1fd97c72b9a6b783db0f2f815dfa7a7ff3
Esdras <esdrasbalves10@gmail.com>
2023-08-21 12:01:49 -0300

    commit

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,16 +1,20 @@
 ﻿// Screen Sound
 using ConsoleApp1;
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 internal class Program
 {
+    static List<string> listaDasBandas = new List<string>();
+
     private static void Main(string[] args)
     {
-        //ExibirMensagemBoasVindas();
-        //ExibirOpocoesDoMenu();
-        Jogo.Game();
+        ExibirOpcoesDoMenu();
+        //Jogo.Game();
+        //ListaPares();
     }
-    static void ExibirMensagemBoasVindas()
+    static void ExibirLogo()
     {
         Console.WriteLine(@"
 ░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
@@ -23,8 +27,9 @@ internal class Program
         Console.WriteLine(mensagemDeBoasVindas);
     }
 
-    static void ExibirOpocoesDoMenu()
+    static void ExibirOpcoesDoMenu()
     {
+        ExibirLogo();
         Console.WriteLine("Digite 1 para registrar uma banda");
         Console.WriteLine("Digite 2 para mostrar todas as bandas");
         Console.WriteLine("Digite 3 para avaliar uma banda");
@@ -37,10 +42,11 @@ internal class Program
 
         switch (opcaoEscolhidaNumerica)
         {
-            case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida); 
+            case 1:
+                RegistrarBanda();
                 break;
             case 2:
-                Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+                MostrarBandasRegistradas();
                 break;
             case 3:
                 Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
@@ -51,9 +57,69 @@ internal class Program
             case -1:
                 Console.WriteLine("Tchau tchau :)");
                 break;
-            default: Console.WriteLine("Opção Inválida.");
+            default:
+                Console.WriteLine("Opção Inválida.");
                 break;
 
+        }
+    }
+
+    static void RegistrarBanda()
+    {
+        Console.Clear();
+        Console.WriteLine("********************");
+        Console.WriteLine("Registro de bandas");
+        Console.WriteLine("********************");
+        Console.Write("Digite o nome da banda que deseja registrar: ");
+        string nomeDaBanda = Console.ReadLine();
+        listaDasBandas.Add(nomeDaBanda);
+
+        Console.WriteLine($"\nA banda {nomeDaBanda} foi registrada com sucesso!");
+        Thread.Sleep(2000);
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+
+    static void MostrarBandasRegistradas()
+    {
+        Console.Clear();
+        Console.WriteLine("**************************************");
+        Console.WriteLine("Exibindo todas as bandas registradas");
+        Console.WriteLine("**************************************\n");
+
+        //for (int i = 0; i<listaDasBandas.Count; i++)
+        //{
+        //    Console.WriteLine($"Banda: {listaDasBandas[i]}");
+        //}
+
+        foreach (string banda in  listaDasBandas)
+        {
+            Console.WriteLine($"Banda: {banda}");
+        }
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu.");
+        Console.ReadKey();
+        Console.Clear();
+        ExibirOpcoesDoMenu();
+    }
+
+    static void ListaPares()
+    {
+        List<int> numbers = new List<int> {1, 2, 3, 4, 5, 6,7, 8, 9, 10};
+
+        for(int i = 0; i < numbers.Count; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+
+        foreach (int number in numbers)
+        {
+            if(number % 2 == 0)
+            {
+                Console.WriteLine(number);
+            }
         }
     }
 }
