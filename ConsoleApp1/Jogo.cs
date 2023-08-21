a5a79475a29d670036b5bdfa59415861c8a5593e
Esdras <esdrasbalves10@gmail.com>
2023-08-21 10:40:15 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Jogo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class Jogo
+    {
+        public static void Game()
+        {
+            var random = new Random();
+            int numeroSecreto = random.Next(1, 101);
+
+            do
+            {
+                Console.WriteLine("Digite um número entre 1 e 100.");
+                int chute = int.Parse(Console.ReadLine());
+
+                if (numeroSecreto == chute)
+                {
+                    Console.WriteLine("\nParabéns, você acertou o número secreto!\n");
+                    break;
+                }
+                else if (chute < numeroSecreto)
+                {
+                    Console.WriteLine("\nO número é maior.\n");
+                }
+                else
+                {
+                    Console.WriteLine("\nO número é menor.\n");
+                }
+            } while (true);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,59 @@
+﻿// Screen Sound
+using ConsoleApp1;
+using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //ExibirMensagemBoasVindas();
+        //ExibirOpocoesDoMenu();
+        Jogo.Game();
+    }
+    static void ExibirMensagemBoasVindas()
+    {
+        Console.WriteLine(@"
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░");
+        string mensagemDeBoasVindas = "\nBoas vindas ao Screen Sound";
+        Console.WriteLine(mensagemDeBoasVindas);
+    }
+
+    static void ExibirOpocoesDoMenu()
+    {
+        Console.WriteLine("Digite 1 para registrar uma banda");
+        Console.WriteLine("Digite 2 para mostrar todas as bandas");
+        Console.WriteLine("Digite 3 para avaliar uma banda");
+        Console.WriteLine("Digite 3 para exibir a média de uma banda");
+        Console.WriteLine("Digite -1 para sair ");
+
+        Console.WriteLine("\nDigite a sua opção: ");
+        string opcaoEscolhida = Console.ReadLine();
+        int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+        switch (opcaoEscolhidaNumerica)
+        {
+            case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhida); 
+                break;
+            case 2:
+                Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+                break;
+            case 3:
+                Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+                break;
+            case 4:
+                Console.WriteLine("Você escolheu a opção " + opcaoEscolhida);
+                break;
+            case -1:
+                Console.WriteLine("Tchau tchau :)");
+                break;
+            default: Console.WriteLine("Opção Inválida.");
+                break;
+
+        }
+    }
+}
